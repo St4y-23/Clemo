08ce61f966a13a1e38abf761e7da4c83671854e6
St4y-23 <ghost19011@gmail.com>
2020-02-04 00:00:20 +0400

    added any blocks

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,14 @@
 $(function () {
 
-  
+  $('.team__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    prevArrow: '<button type="button" class="slick-arrow slick-prev"><svg class="arrow__icon"><use xlink:href="#arrow"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-arrow slick-next"><svg class="arrow__icon"><use xlink:href="#arrow"></use></svg></button>',
+  });
+
 
 });
