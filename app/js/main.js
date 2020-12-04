9d5413c7bf5f42cee0278a410982de7d8d8de9b0
Vladislav Novgorodtsev <ghost19011@gmail.com>
2020-12-04 07:46:36 +0400

    update

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,15 @@
 $(function () {
 
+  
+
+
+  // MENU
+  $('.ham').on('click', function(){
+    $('.nav').slideToggle();
+  })
+
+
+
   $('.team__slider').slick({
     infinite: true,
     slidesToShow: 3,
@@ -8,6 +18,24 @@ $(function () {
     dots: true,
     prevArrow: '<button type="button" class="slick-arrow slick-prev"><svg class="arrow__icon"><use xlink:href="#arrow"></use></svg></button>',
     nextArrow: '<button type="button" class="slick-arrow slick-next"><svg class="arrow__icon"><use xlink:href="#arrow"></use></svg></button>',
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          arrows: false,
+          dots: true,
+        }
+      },
+      {
+        breakpoint: 830,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+          dots: true,
+          fade: true,
+        }
+      },
+    ]
   });
 
   $('.testimonials__sliderTop').slick({
@@ -24,7 +52,17 @@ $(function () {
     asNavFor: '.testimonials__sliderTop',
     dots: true,
     arrows: false,
-    focusOnSelect: true
+    focusOnSelect: true,
+    autoplay: true,
+    autoplaySpeed: 7000,
+    responsive: [
+      {
+        breakpoint: 830,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
   });
 
   $('.work__slider').slick({
@@ -34,8 +72,18 @@ $(function () {
     arrows: true,
     fade: true,
     adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 7000,
     prevArrow: '<button type="button" class="work__arrow work-prev">prev</button>',
     nextArrow: '<button type="button" class="work__arrow work-next">next</button>',
+    responsive: [
+      {
+        breakpoint: 830,
+        settings: {
+          arrows: false,
+        }
+      },
+    ]
   });
 
   $('.related__slider').slick({
@@ -43,7 +91,19 @@ $(function () {
     slidesToScroll: 1,
     dots: true,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 1130,
+        settings: {
+          slidesToShow: 1,
+          fade: true,
+        }
+      },
+    ]
   });
+
+  
+
 
 
   
